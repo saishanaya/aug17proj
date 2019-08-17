34fcbcf8fa0524e26db80aaeb91d4cdfb1704a94
saishanaya <54216584+saishanaya@users.noreply.github.com>
2019-08-17 12:55:43 -0500

    initial push

---
+++ aug17/src/main/java/customer/java.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customer;
+
+/**
+ *
+ * @author Hp User
+ */
+public class java {
+    Customer cust;
+    public java() {
+        
+    }
+
+    private static class Customer {
+
+        public Customer() {
+        }
+    }
+}
